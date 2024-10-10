369ff2cc4927b8b62d60d33c5c6b29788896f219
Christopher <voschristopher53@gmail.com>
2024-10-10 20:01:50 +0200

    added pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-/**
- * Challenge:
- * Bootstrap the VanLife project by creating the first 2 routes:
- * Home and About.
- * 
- * Also include the navbar that can link between the two routes.
- * For now, you'll either need to copy/paste the navbar code
- * to both Home and About pages, or you'll need to find a place
- * to put it where it can be shared between the two pages.
- * (Don't overthink this part - just do whatever is easiest for
- * you because we'll learn a better approach very soon)
- * 
- * Review challenge: do all the CSS yourself based on the design
- * linked in the slides.
- */
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Vans from "./pages/Vans";
+
+import "../server";
 
 function App() {
-
   return (
-    <h1>Start here</h1>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
